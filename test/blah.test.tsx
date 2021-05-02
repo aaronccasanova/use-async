db166fd04a3c072987e3d814c0988ae6571d5072
Aaron Casanova <aaronccasanova@gmail.com>
2021-05-02 13:13:05 -0700

    chore: misc updates

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,17 @@
+// TODO: Write tests
+
 import React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Default as Thing } from '../stories/Thing.stories'
+// import { Default as Thing } from '../stories/Thing.stories'
 
-describe('Thing', () => {
+function Placeholder() {
+  return null
+}
+
+describe('useAsync', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Thing />, div)
+    ReactDOM.render(<Placeholder />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
